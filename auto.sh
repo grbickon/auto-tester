bfb636eddf39832668a0f0ba7b6969a78c5957c1
grbkons <59757529+grbkons@users.noreply.github.com>
2020-01-11 20:27:20 -0500

    Update auto.sh
    Added handling of multi-line outputs, a help flag, and did some refactoring.

--- auto.sh
+++ auto.sh
@@ -1,44 +1,62 @@
 #!/usr/bin/env bash
 
+check_make () {
+  if [ ! -e "Makefile" ]
+    then
+      echo Makefile missing
+      exit 1
+  fi
+}
+
 if [ "$#" -lt "1" ]
   then
     echo At least 1 arguments required
     exit 1
 fi
 
-while getopts ":c" option;
+
+
+while getopts ":ach" option;
 do
   case $option in 
-    c)
-      if [  -e "Makefile" ]
-        then
-          echo Makefile missing
-          exit 1
-      fi
-      make clean
+    a)
+      check_make
       make all
-      shift
+      echo 
+      ;;
+    c)
+      check_make
+      make clean
+      echo 
+      ;;
+    h)
+      echo 'A bash script for automating testing to standard out'
+      echo "  -a(ll) runs 'make all' before running tests"
+      echo "  -c(lean) runs 'make clean' before running tests"
+      echo "  -h(elp) displays all flags and their descriptions"
+      echo 
       ;;
     *)
-      echo "invalid option -$OPTARG"
+      echo "invalid option -$option"
       ;;
   esac
 done
 
+shift $(($OPTIND -1))
+
 while (( $# ))
   do
-    echo You gave me $1
     if [ -e $1 ]
       then
-      exp=`awk '{if(NR==2) print $0}' $1`
+      exp=$(awk '{if(NR!=1) print $0}' $1)
       rec=$($(awk '{if(NR==1) print $0}' $1))
-      echo $exp
-      echo $rec
-      if [ $exp = $rec ]
-        then echo Test $1 passed
-        else echo Expected hello, Recieved ./$1
+      if [ "$exp" = "$rec" ]
+        then echo "Test '$1' passed"
+        else echo "Test '$1' failed"
+             echo "Expected; $exp"
+             echo "Recieved; $rec"
       fi
-      else echo $1 does not exist
+      else echo Test \'$1\' does not exist
     fi
     shift
   done
